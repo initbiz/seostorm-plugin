e836bcb4a9b019e6bcf4769187f7dae4995d9864
Tomasz Strojny <tomasz@init.biz>
2020-10-22 13:07:48 +0200

    Event to extend model classes to be handled by storm

--- eventhandlers/SeoStormedModelsHandler.php
+++ eventhandlers/SeoStormedModelsHandler.php
@@ -2,17 +2,35 @@
 
 namespace Initbiz\SeoStorm\EventHandlers;
 
+use Event;
 use Initbiz\SeoStorm\Models\SeoOptions;
 
 class SeoStormedModelsHandler
 {
+    /**
+     * Array of model classes to extend
+     *
+     * @var array
+     */
+    protected $modelClasses = [];
+
     public function subscribe($event)
     {
-        $modelClasses = [
-            '\RainLab\Blog\Models\Post',
-        ];
+        $this->addModelClass('\RainLab\Blog\Models\Post');
 
-        foreach ($modelClasses as $modelClass) {
+        /*
+         * @event initbiz.seostorm.modelsHandler.listClasses
+         * Gives the ability to manage the model classes attribute dynamically
+         *
+         * Example usage to add a custom class for the extension:
+         *
+         * Event::listen('initbiz.seostorm.modelsHandler.listClasses', function($handler) {
+         *     $handler->addModelClass(Model::class);
+         * });
+         */
+        Event::fire('initbiz.seostorm.modelsHandler.listClasses', [$this]);
+
+        foreach ($this->modelClasses as $modelClass) {
             try {
                 $model = new $modelClass;
             } catch (\Throwable $th) {
@@ -25,7 +43,7 @@ class SeoStormedModelsHandler
                 }
             });
 
-            SeoOptions::extend(function($model) use ($modelClass) {
+            SeoOptions::extend(function ($model) use ($modelClass) {
                 $model->morphTo['seostorm_options'][] = [
                     $modelClass,
                     'name' => 'stormed',
@@ -33,5 +51,10 @@ class SeoStormedModelsHandler
                 ];
             });
         }
+    }
+
+    public function addModelClass(string $className)
+    {
+        $this->modelClasses[] = $className;
     }
 }
