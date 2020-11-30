15e9c7df0e20f26711859aec05c085eda7b07794
Patryk <patryk@init.biz>
2020-11-30 18:28:43 +0100

    cls

--- components/Seo.php
+++ components/Seo.php
@@ -84,7 +84,6 @@ class Seo extends ComponentBase
             $this->page['viewBag']->setProperties($properties);
         }
         $this->disable_schema = $this->property('disable_schema');
-        $this->getTitle();
     }
 
     /**
