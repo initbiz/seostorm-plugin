80b7c2e01ee4f26704b5bf78101482c4bff4e926
Tomasz Strojny <tomasz@init.biz>
2024-09-11 11:02:35 +0200

    404 when no file found

--- controllers/FaviconController.php
+++ controllers/FaviconController.php
@@ -3,17 +3,21 @@
 namespace Initbiz\SeoStorm\Controllers;
 
 use Response;
+use System\Models\File;
 use Initbiz\SeoStorm\Models\Settings;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class FaviconController
 {
-    public function faviconIco(): BinaryFileResponse
+    public function faviconIco()
     {
         $settings = Settings::instance();
 
         $favicon = $settings->getFaviconObject();
 
-        return Response::file($favicon->getLocalPath(), ['Content-Type' => $favicon->getContentType()]);
+        if ($favicon instanceof File) {
+            return Response::file($favicon->getLocalPath(), ['Content-Type' => $favicon->getContentType()]);
+        }
+
+        return Response::make('Not found', 404);
     }
 }
